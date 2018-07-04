452de481d8e5c4e1afe076ab12d0c2d73fe6ef48
Sarah Geyer <sjgeyer@comcast.net>
2018-07-03 20:47:26 -0700

    add app listen and port

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.static(`${__dirname}/build`));
+
+app.listen(process.env.PORT, () => {
+  console.log('__SERVER UP__', process.env.PORT);
+});
 
 app.get('*', (request, response) => {
   response.sendFile(`${__dirname}/build/index.html`);
