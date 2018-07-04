63ebd09832791b88a7307894118e84d25cb594a9
Sarah Geyer <sjgeyer@comcast.net>
2018-07-03 20:17:52 -0700

    contact page done for now

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './contact.scss';
+import me from '../../assets/me.jpg';
 
 export default class Contact extends React.Component {
   render() {
     return (
       <div className='contact'>
-        <h1>Contact Me</h1>
-        <h3><a href='https://www.linkedin.com/in/sarahgeyer/'>LinkedIn</a></h3>
-        <h3><a href='https://github.com/sjgeyer'>GitHub</a></h3>
-        <h3>Email: sjaygeyer@gmail.com</h3>
+        <div className='blurb'>
+          <img src={me} alt='me'/>
+          <p>If you like what you see, I would love to hear from you!
+            Find me at the following places:</p>
+          <a href='https://www.linkedin.com/in/sarahgeyer/'><button>LinkedIn</button></a>
+          <a href='https://github.com/sjgeyer'><button>GitHub</button></a>
+        </div>
       </div>
     );
   }
